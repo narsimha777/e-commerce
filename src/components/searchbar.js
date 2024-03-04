e421dc570087e0645dea4eb7f295933728c5303e
narsimha777 <kandinarsimhareddy09@gmail.com>
2024-03-04 17:07:10 +0530

    carterrdebugged

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink} from "react-router-dom";
 import ROUTES from "../routes";
 import logo from '../img/logo.png';
 import { logout } from "../utils/requests";
@@ -33,7 +33,7 @@ export default function Searchbar({setSearch, logged, setLogged}){
                     <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z" />
                 </svg>
                 {logged&&<li className="nav-item">
-                    <NavLink className="nav-link active" aria-current="page" to={ROUTES.cart(id)}>Cart</NavLink>
+                    <NavLink className="nav-link active" aria-current="page" onClick={()=>{history.pushState({},"", ROUTES.cart(id)); location.reload()}} to={ROUTES.cart(id)}>Cart</NavLink>
                 </li>}
                 {logged&&name&&<li className="nav-item">
                     <a className="nav-link active" aria-current="page" href="#" style={{color:"orange"}}>{name}</a>
